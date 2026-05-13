bfe6fe4d6d2dbdd2fbeb1edc0fbf72d95204442b
shopia-sal <shopiasalma599@gmail.com>
2026-05-13 16:50:07 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\ASUS\\SDK\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ASUS\\PakGege_Semester6\\project_keenam" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\ASUS\\SDK\\flutter"
+  "PROJECT_DIR=C:\\Users\\ASUS\\PakGege_Semester6\\project_keenam"
+  "FLUTTER_ROOT=C:\\Users\\ASUS\\SDK\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ASUS\\PakGege_Semester6\\project_keenam\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ASUS\\PakGege_Semester6\\project_keenam"
+  "FLUTTER_TARGET=C:\\Users\\ASUS\\PakGege_Semester6\\project_keenam\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049Y2MwNzM0YWM3MQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NTlhYTU4NGZkZg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS41"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ASUS\\PakGege_Semester6\\project_keenam\\.dart_tool\\package_config.json"
+)
